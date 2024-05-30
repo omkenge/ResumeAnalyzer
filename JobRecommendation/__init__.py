cae66bc647bc90b701531be5fde742c398da4830
Om Kenge <88768848+omkenge@users.noreply.github.com>
2024-05-31 01:53:45 +0530

    Update __init__.py

--- JobRecommendation/__init__.py
+++ JobRecommendation/__init__.py
@@ -1,4 +1,1 @@
-from dotenv import load_dotenv
-print(f"Loading environment variable from .env file")
-load_dotenv()
-# This make sure that all the envirinment variables is getting read when ever  we are calling the code 
+
